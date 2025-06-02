3291634d14d1bebf631c6c078d1849daaaa92c79
Pankaj-Suthar-25 <ps25softwaredeveloper@gmail.com>
2025-06-02 22:01:57 +0530

    Interactive Tab Bar

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ModernTabBar(),
+      home: const InteractiveTabBar(),
     );
   }
 }
@@ -138,6 +138,129 @@ class _ModernTabBarState extends State<ModernTabBar>
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class InteractiveTabBar extends StatefulWidget {
+  const InteractiveTabBar({super.key});
+
+  @override
+  State<InteractiveTabBar> createState() => _InteractiveTabBarState();
+}
+
+class _InteractiveTabBarState extends State<InteractiveTabBar>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+  String _screen1Text = 'Welcome to Screen 1';
+  String _screen2Text = 'Welcome to Screen 2';
+  double _scale = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _changeText(int tabIndex) {
+    setState(() {
+      if (tabIndex == 0) {
+        _screen1Text = 'You tapped on Screen 1!';
+      } else {
+        _screen2Text = 'You tapped on Screen 2!';
+      }
+    });
+  }
+
+  void _animateText() {
+    setState(() {
+      _scale = 1.2;
+    });
+    Future.delayed(const Duration(milliseconds: 100), () {
+      setState(() {
+        _scale = 1.0;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Interactive Tab Bar'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(icon: Icon(Icons.home), text: 'Home'),
+            Tab(icon: Icon(Icons.settings), text: 'Settings'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          GestureDetector(
+            onTap: () {
+              _changeText(0);
+              _animateText();
+            },
+            child: Center(
+              child: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                child: ScaleTransition(
+                  scale: Tween<double>(begin: 1.0, end: _scale)
+                      .animate(CurvedAnimation(
+                    parent: AnimationController(
+                      vsync: this,
+                      duration: const Duration(milliseconds: 300),
+                    ),
+                    curve: Curves.easeInOut,
+                  )),
+                  child: Text(
+                    _screen1Text,
+                    key: ValueKey<String>(_screen1Text),
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              _changeText(1);
+              _animateText();
+            },
+            child: Center(
+              child: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                child: ScaleTransition(
+                  scale: Tween<double>(begin: 1.0, end: _scale)
+                      .animate(CurvedAnimation(
+                    parent: AnimationController(
+                      vsync: this,
+                      duration: const Duration(milliseconds: 300),
+                    ),
+                    curve: Curves.easeInOut,
+                  )),
+                  child: Text(
+                    _screen2Text,
+                    key: ValueKey<String>(_screen2Text),
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
